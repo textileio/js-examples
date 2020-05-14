2012ae9a8b6dbe0bddc45d41ce05d90d1a6ebf5a
andrew <andrew@textile.io>
2020-05-13 18:33:16 -0700

    fix(build): threadsid hack added
    Signed-off-by: andrew <andrew@textile.io>

--- react-native-hub-app/.eslintrc.js
+++ react-native-hub-app/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    useJSXTextNode: true,
+    project: './tsconfig.json',
+    tsconfigRootDir: '.',
+  },
+  plugins: ['plugin:@typescript-eslint/recommended'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+}
