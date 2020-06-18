5d87e27a284e40efa3d972b608809e611bfd945c
Andrew Hill <andrew@textile.io>
2020-06-18 14:22:06 -0700

    fix(lint): update ts a bit
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- react-native-hub-app/src/checklist.tsx
+++ react-native-hub-app/src/checklist.tsx
@@ -6,27 +6,28 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from 'react'
+import { FlatList, View, Text, TouchableOpacity, Linking } from 'react-native'
+// @ts-ignore
+import Prompt from 'react-native-input-prompt'
+// @ts-ignore
+import { USER_API_SECRET, USER_API_KEY } from 'react-native-dotenv'
+// @ts-ignore
+import Filter from 'bad-words'
+import { Where } from '@textile/threads-client'
+import { Buckets, Client, KeyInfo, ThreadID } from '@textile/hub'
 import {
-  FlatList,
-  View,
-  Text,
-  TouchableOpacity,
-  Linking,
-} from 'react-native';
-// @ts-ignore
-import Prompt from 'react-native-input-prompt';
-// @ts-ignore
-import {USER_API_SECRET, USER_API_KEY} from 'react-native-dotenv';
-// @ts-ignore
-import Filter from 'bad-words';
-import {Where} from '@textile/threads-client';
-import {Buckets, Client, KeyInfo, ThreadID} from '@textile/hub';
-import {createAstronaut, generateWebpage, generateIdentity, getCachedUserThread, cacheUserThread, astronautSchema} from './helpers';
-import styles from './styles';
+  createAstronaut,
+  generateWebpage,
+  generateIdentity,
+  getCachedUserThread,
+  cacheUserThread,
+  astronautSchema,
+} from './helpers'
+import styles from './styles'
 
-const MAX_STEPS = 2;
-const sleep = (m: number) => new Promise((r) => setTimeout(r, m));
+const MAX_STEPS = 2
+const sleep = (m: number) => new Promise((r) => setTimeout(r, m))
 
 interface StateProps {
   steps: any
@@ -47,12 +48,12 @@ class CheckList extends React.Component<StateProps> {
   // you could also do this, so no constructor needed
   state: StateProps = {
     steps: [
-      {key: 'Step 0', name: 'Prepare Identity & Token', status: 0},
-      {key: 'Step 1', name: 'Setup ThreadDB', status: 0},
-      {key: 'Step 2', name: 'Add Instance to Collection', status: 0},
-      {key: 'Step 3', name: 'Query from our Collection', status: 0},
-      {key: 'Step 4', name: 'Create a webpage', status: 0},
-      {key: 'Step 5', name: 'Push webpage to User Bucket', status: 0},
+      { key: 'Step 0', name: 'Prepare Identity & Token', status: 0 },
+      { key: 'Step 1', name: 'Setup ThreadDB', status: 0 },
+      { key: 'Step 2', name: 'Add Instance to Collection', status: 0 },
+      { key: 'Step 3', name: 'Query from our Collection', status: 0 },
+      { key: 'Step 4', name: 'Create a webpage', status: 0 },
+      { key: 'Step 5', name: 'Push webpage to User Bucket', status: 0 },
     ],
     step: 0,
     errorMessage: '',
@@ -61,40 +62,40 @@ class CheckList extends React.Component<StateProps> {
     promptHint: '',
     identity: '',
     db: new Client(),
-  };
+  }
 
   incrementStatus(index: number) {
-    const steps = this.state.steps;
-    const data = steps[index];
+    const steps = this.state.steps
+    const data = steps[index]
     if (data.status >= MAX_STEPS) {
-      return;
+      return
     }
-    data.status = data.status += 1;
-    steps[index] = data;
+    data.status = data.status += 1
+    steps[index] = data
     this.setState({
       steps: steps,
-    });
+    })
   }
 
   async runStep(stepNumber: number) {
-    const steps = this.state.steps;
-    const data = steps[stepNumber];
+    const steps = this.state.steps
+    const data = steps[stepNumber]
     try {
-      let {db, threadId} = this.state;
+      let { db, threadId } = this.state
       switch (stepNumber) {
         case 0: {
           /**
            * Create a new user Identity
-           * 
+           *
            * The identity pk will be cached in AsyncStorage.
            * On the next session, the pk identity will be reused
            */
-          const id = await generateIdentity();
-          const identity = id.toString();
-        
+          const id = await generateIdentity()
+          const identity = id.toString()
+
           /**
            * Authenticate the user with your User Key and Secret
-           * 
+           *
            * This will allow the user to store threads and buckets
            * using your developer resources on the Hub.
            */
@@ -106,133 +107,131 @@ class CheckList extends React.Component<StateProps> {
 
           /**
            * Auth our Database with admin info
-           * 
+           *
            * API calls will now include the credentials created above
            */
           db = await Client.withKeyInfo(info)
           /**
            * Generate an app user API token
-           * 
+           *
            * The app user (defined by Identity) needs an API token
            * The API will give you one based on ID plus Credentials
-           * 
+           *
            * The token will be added to the existing db.context.
            */
-          await db.getToken(id);
+          await db.getToken(id)
 
           data.message = 'Created new Identity'
 
-          data.status = 2;
-          steps[stepNumber] = data;
+          data.status = 2
+          steps[stepNumber] = data
           this.setState({
             identity,
             db: db,
-          });
+          })
 
-          break;
+          break
         }
         case 1: {
           /**
            * An app should create a minimum number of Threads per user
-           * 
-           * A single Thread can contain a large number of distinct 
+           *
+           * A single Thread can contain a large number of distinct
            * Collections for different types of data.
-           * 
-           * Here, we create or restore the user's 
+           *
+           * Here, we create or restore the user's
            */
-          let threadId = await getCachedUserThread();
+          threadId = await getCachedUserThread()
 
           /**
            * Setup a new ThreadID and Database
            */
           if (!threadId) {
-            threadId = ThreadID.fromRandom();
-            await cacheUserThread(threadId);
+            threadId = ThreadID.fromRandom()
+            await cacheUserThread(threadId)
             /**
              * Each new ThreadID requires a `newDB` call.
              */
             await db.newDB(threadId)
-  
-            /** 
+
+            /**
              * We add our first Collection to the DB for Astronauts.
              */
-            await db.newCollection(threadId, 'Astronaut', astronautSchema);
+            await db.newCollection(threadId, 'Astronaut', astronautSchema)
           }
 
           /**
            * Update our context with the target threadId.
            */
-          db.context.withThread(threadId.toString());
+          db.context.withThread(threadId.toString())
 
           // Update our app state with success
-          data.status = 2;
+          data.status = 2
           data.message = 'User Thread linked to Identity'
-          steps[stepNumber] = data;
+          steps[stepNumber] = data
           this.setState({
             db,
             threadId,
             steps,
-          });
-          break;
+          })
+          break
         }
         case 2: {
           /**
            * Add a new Astronaut
-           * 
+           *
            * Our Thread contains the Astronaut Collection, so you just need
            * to add a new astronaut that matches the expected schema.
-           * 
+           *
            * If you run this app many times, you'll notice many Buzz Aldrin
            * entries in your ThreadDB, each with a unique ID.
            */
-          const ids = await db.create(threadId!, 'Astronaut', [
-            createAstronaut(),
-          ]);
+          const ids = await db.create(threadId!, 'Astronaut', [createAstronaut()])
 
           // Update our app state with success
-          data.status = 2;
-          steps[stepNumber] = data;
-          data.message = `New instance added: ${ids[0]}`;
+          data.status = 2
+          steps[stepNumber] = data
+          data.message = `New instance added: ${ids[0]}`
           this.setState({
             entityId: ids[0],
             steps: steps,
-          });
-          break;
+          })
+          break
         }
         case 3: {
           /**
            * You can search all your existing Buzz Aldrins
            */
-          const q = new Where('firstName').eq('Buzz');
-          const r = await db.find(threadId!, 'Astronaut', q);
+          const q = new Where('firstName').eq('Buzz')
+          const r = await db.find(threadId!, 'Astronaut', q)
 
           const ids = r.instancesList.map((instance: any) => instance._id)
 
           /**
            * Clean up our entries (just delete them all!)
            */
-          await db.delete(threadId!, 'Astronaut', ids);
+          await db.delete(threadId!, 'Astronaut', ids)
 
-          data.status = ids.indexOf(this.state.entityId) > -1 ? 2 : 9;
-          data.message = `${ids.length} existing instances found`;
-          steps[stepNumber] = data;
+          data.status = ids.indexOf(this.state.entityId) > -1 ? 2 : 9
+          data.message = `${ids.length} existing instances found`
+          steps[stepNumber] = data
           this.setState({
             steps: steps,
-          });
-          break;
+          })
+          break
         }
         case 4: {
           /**
            * Buckets
-           * 
+           *
            * You can now create Buckets for your User.
            * Bucket will contain raw files and documents.
-           * 
+           *
            * We'll use the same Context we setup for the ThreadDB.
            */
-          const buckets = new Buckets(db.context);
+          const buckets = new Buckets(db.context)
 
-          const roots = await buckets.list();
+          const roots = await buckets.list()
           const existing = roots.find((bucket) => bucket.name === 'files')
 
           /**
@@ -241,38 +240,40 @@ class CheckList extends React.Component<StateProps> {
            */
           let bucketKey = ''
           if (existing) {
-            bucketKey = existing.key;
+            bucketKey = existing.key
           } else {
-            const created = await buckets.init('files');
-            bucketKey = created.root!.key;
+            const created = await buckets.init('files')
+            bucketKey = created.root!.key
           }
 
           this.setState({
             bucketKey,
             promptTitle: existing ? 'Update your Website' : 'Create new Website',
-            promptHint: existing ? 'Give it a new name, like "Fakeblock"' : 'Give it a name, like "Rainbows and Cupcakes"',
+            promptHint: existing
+              ? 'Give it a new name, like "Fakeblock"'
+              : 'Give it a name, like "Rainbows and Cupcakes"',
             showPrompt: true,
-          });
-          break;
+          })
+          break
         }
         case 5: {
-          const {bucketKey} = this.state;
-          
+          const { bucketKey } = this.state
+
           /**
            * Still using the same context.
            */
-          const buckets = new Buckets(db.context);
+          const buckets = new Buckets(db.context)
 
           /**
            * Create a simple html string for the webpage
            */
-          const webpage = generateWebpage(this.state.content || '');
+          const webpage = generateWebpage(this.state.content || '')
 
           /**
            * Add a simple file Buffer
-           * 
+           *
            * Alternative formats are here: https://github.com/textileio/js-textile/blob/master/src/normalize.ts#L14
-           * 
+           *
            * We add the file as index.html so that we can render it right in the browser afterwards.
            */
           const file = { path: '/index.html', content: Buffer.from(webpage) }
@@ -281,70 +282,70 @@ class CheckList extends React.Component<StateProps> {
            * Push the file to the root of the Files Bucket.
            */
           const raw = await buckets.pushPath(bucketKey!, 'index.html', file)
-          
-          data.status = 2;
+
+          data.status = 2
           this.setState({
             ipfsAddr: raw.root,
             steps: steps,
-          });
-          break;
+          })
+          break
         }
         default:
-          data.status = 9;
-          steps[stepNumber] = data;
+          data.status = 9
+          steps[stepNumber] = data
           this.setState({
             steps: steps,
-          });
-          return;
+          })
+          return
       }
     } catch (err) {
-      data.status = 9;
-      data.message = err.message;
-      steps[stepNumber] = data;
+      data.status = 9
+      data.message = err.message
+      steps[stepNumber] = data
       this.setState({
         steps: steps,
-      });
+      })
     }
   }
 
   async runAllSteps(stepNumber: number) {
-    await sleep(1200); // <- just adds a delay between steps for UI looks
-    this.runStep(stepNumber);
+    await sleep(1200) // <- just adds a delay between steps for UI looks
+    this.runStep(stepNumber)
   }
 
   showStatus(stepNumber: number) {
-    const steps = this.state.steps;
-    const data = steps[stepNumber];
-    let message = JSON.stringify(data.message);
+    const steps = this.state.steps
+    const data = steps[stepNumber]
+    let message = JSON.stringify(data.message)
     if (!message) {
       switch (data.status) {
         case 0: {
-          message = 'step pending';
-          break;
+          message = 'step pending'
+          break
         }
         case 1: {
-          message = 'step running';
-          break;
+          message = 'step running'
+          break
         }
         case 2: {
-          message = 'step success';
-          break;
+          message = 'step success'
+          break
         }
         default: {
-          message = 'step failed';
-          break;
+          message = 'step failed'
+          break
         }
       }
     }
-    this.setState({errorMessage: message});
+    this.setState({ errorMessage: message })
   }
   renderRow(value: any) {
-    const {item, index} = value;
+    const { item, index } = value
     if (item.status === 0 && this.state.step === index) {
-      this.runAllSteps(index);
-      this.incrementStatus(index);
+      this.runAllSteps(index)
+      this.incrementStatus(index)
     } else if (item.status === 2 && this.state.step === index) {
-      this.setState({step: this.state.step + 1});
+      this.setState({ step: this.state.step + 1 })
     }
     const status =
       item.status === 2
@@ -353,51 +354,40 @@ class CheckList extends React.Component<StateProps> {
         ? 'error'
         : this.state.step === index
         ? 'running'
-        : 'pending';
+        : 'pending'
 
     const statusText =
-      item.status < 1
-        ? ' ▵ '
-        : item.status === 1
-        ? ' ★ '
-        : item.status === 2
-        ? ' ✓ '
-        : ' ✘ ';
+      item.status < 1 ? ' ▵ ' : item.status === 1 ? ' ★ ' : item.status === 2 ? ' ✓ ' : ' ✘ '
 
     const iconColor =
-      item.status < 1
-        ? 'grey'
-        : item.status === 1
-        ? 'orange'
-        : item.status === 2
-        ? 'green'
-        : 'red';
+      item.status < 1 ? 'grey' : item.status === 1 ? 'orange' : item.status === 2 ? 'green' : 'red'
 
     return (
       <TouchableOpacity
         style={styles.row}
         onPress={() => {
-          this.showStatus(index);
-        }}>
-        <View style={styles.row_cell_timeplace}>
-          <Text style={styles.row_time}>{`${item.key}`}</Text>
-          <Text style={styles.row_name}>{item.name}</Text>
+          this.showStatus(index)
+        }}
+      >
+        <View style={styles.rowCellTimeplace}>
+          <Text style={styles.rowTime}>{`${item.key}`}</Text>
+          <Text style={styles.rowName}>{item.name}</Text>
         </View>
 
-        <Text style={styles.row_cell_temp}>{status}</Text>
+        <Text style={styles.rowCellTemp}>{status}</Text>
         <View>
-          <Text style={{color: iconColor}}>{statusText}</Text>
+          <Text style={{ color: iconColor }}>{statusText}</Text>
         </View>
       </TouchableOpacity>
-    );
+    )
   }
 
   websiteCancelled() {
-    const steps = this.state.steps;
-    const data = steps[4];
-    data.status = 9;
+    const steps = this.state.steps
+    const data = steps[4]
+    data.status = 9
     data.message = 'User cancelled'
-    steps[4] = data;
+    steps[4] = data
     this.setState({
       steps: steps,
       showPrompt: false,
@@ -405,21 +395,21 @@ class CheckList extends React.Component<StateProps> {
   }
 
   websiteNamed(title: string) {
-    const steps = this.state.steps;
-    const data = steps[4];
-    const filter = new Filter();
+    const steps = this.state.steps
+    const data = steps[4]
+    const filter = new Filter()
     let content = title
     if (filter.isProfane(content)) {
       content = filter.clean(content)
     }
-    data.status = 2;
-    steps[4] = data;
+    data.status = 2
+    steps[4] = data
     this.setState({
       steps: steps,
       showPrompt: false,
       content,
     })
-  } 
+  }
 
   render() {
     return (
@@ -434,26 +424,34 @@ class CheckList extends React.Component<StateProps> {
           <Text style={styles.error}>{this.state.errorMessage}</Text>
         </View>
         <View>
-          <Text style={{...styles.error, ...{color: 'blue'}}}
-                onPress={() => Linking.openURL(`https://${this.state.bucketKey}.textile.space`)}>
+          <Text
+            style={{ ...styles.error, ...{ color: 'blue' } }}
+            onPress={() => Linking.openURL(`https://${this.state.bucketKey}.textile.space`)}
+          >
             {this.state.ipfsAddr ? 'IPNS Webpage Link' : ''}
           </Text>
         </View>
         <View>
-          <Text style={{...styles.error, ...{color: 'blue'}}}
-                onPress={() => Linking.openURL(`https://${this.state.threadId}.thread.hub.textile.io/buckets/${this.state.bucketKey}`)}>
+          <Text
+            style={{ ...styles.error, ...{ color: 'blue' } }}
+            onPress={() =>
+              Linking.openURL(
+                `https://${this.state.threadId}.thread.hub.textile.io/buckets/${this.state.bucketKey}`,
+              )
+            }
+          >
             {this.state.ipfsAddr ? 'Thread Bucket Link' : ''}
           </Text>
         </View>
         <Prompt
-            visible={this.state.showPrompt}
-            title={this.state.promptTitle}
-            placeholder={this.state.promptHint}
-            onCancel={() => this.websiteCancelled()}
-            onSubmit={(text: string) => this.websiteNamed(text)}
+          visible={this.state.showPrompt}
+          title={this.state.promptTitle}
+          placeholder={this.state.promptHint}
+          onCancel={() => this.websiteCancelled()}
+          onSubmit={(text: string) => this.websiteNamed(text)}
         />
       </View>
-    );
+    )
   }
 }
-export default CheckList;
+export default CheckList
--- react-native-hub-app/src/helpers.tsx
+++ react-native-hub-app/src/helpers.tsx
@@ -1,44 +1,42 @@
-import {
-  AsyncStorage,
-} from 'react-native';
-import {ThreadID} from '@textile/hub';
-import {Libp2pCryptoIdentity} from '@textile/threads-core';
+import { AsyncStorage } from 'react-native'
+import { ThreadID } from '@textile/hub'
+import { Libp2pCryptoIdentity } from '@textile/threads-core'
 
 const version = 10001 //Math.floor(Math.random() * 1000);
-const IDENTITY_KEY = 'identity-' + version;
-const USER_THREAD_ID = 'user-thread-' + version;
+const IDENTITY_KEY = 'identity-' + version
+const USER_THREAD_ID = 'user-thread-' + version
 
 export const cacheUserThread = async (id: ThreadID) => {
-  await AsyncStorage.setItem(USER_THREAD_ID, id.toString());
+  await AsyncStorage.setItem(USER_THREAD_ID, id.toString())
 }
 
 export const getCachedUserThread = async (): Promise<ThreadID | undefined> => {
   /**
    * All storage should be scoped to the identity
-   * 
+   *
    * If the identity changes and you try to use an old database,
    * it will error due to not authorized.
    */
-  let idStr = await AsyncStorage.getItem(USER_THREAD_ID);
+  const idStr = await AsyncStorage.getItem(USER_THREAD_ID)
   if (idStr) {
     /**
      * Temporary hack to get ThreadID working in RN
      */
-    const id: ThreadID = ThreadID.fromString(idStr);
-    return id;
-  } 
-  return undefined;
+    const id: ThreadID = ThreadID.fromString(idStr)
+    return id
+  }
+  return undefined
 }
 
 export const generateIdentity = async (): Promise<Libp2pCryptoIdentity> => {
-  let idStr = await AsyncStorage.getItem(IDENTITY_KEY);
+  let idStr = await AsyncStorage.getItem(IDENTITY_KEY)
   if (idStr) {
-    return await Libp2pCryptoIdentity.fromString(idStr);
+    return await Libp2pCryptoIdentity.fromString(idStr)
   } else {
-    const id = await Libp2pCryptoIdentity.fromRandom();
-    idStr = id.toString();
-    await AsyncStorage.setItem(IDENTITY_KEY, idStr);
-    return id;
+    const id = await Libp2pCryptoIdentity.fromRandom()
+    idStr = id.toString()
+    await AsyncStorage.setItem(IDENTITY_KEY, idStr)
+    return id
   }
 }
 
@@ -67,7 +65,7 @@ export const astronautSchema = {
       minimum: 0,
     },
   },
-};
+}
 
 export const createAstronaut = () => {
   return {
@@ -75,8 +73,8 @@ export const createAstronaut = () => {
     firstName: 'Buzz',
     lastName: 'Aldrin',
     missions: 2,
-  };
-};
+  }
+}
 
 export const generateWebpage = (title: string) => {
   return `<!doctype html><html lang=en><meta charset=utf-8><meta name=viewport content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1"><title>${title}</title><link rel=apple-touch-icon sizes=180x180 href=https://hub.textile.io/public/img/apple-touch-icon.png><link rel=icon type=image/png sizes=32x32 href=https://hub.textile.io/public/img/favicon-32x32.png><link rel=icon type=image/png sizes=16x16 href=https://hub.textile.io/public/img/favicon-16x16.png><style>html,body{margin:0;padding:0;background:#333;font-family:Arial,Geneva,sans-serif;font-size:30px}@media(min-width:600px){body{font-size:40px}}@media(min-width:800px){body{font-size:60px}}@media(min-width:1000px){body{font-size:75px}}.title{position:fixed;top:30%;left:50%;transform:translate(-50%,-30%);color:#ddd;text-align:center}.link{position:absolute;bottom:1%;right:2%;z-index:1}.link a{font-size:10px;color:#777;text-decoration:none}.background{position:absolute;display:block;top:0;left:0;bottom:0;right:0;width:100%;height:100%;z-index:0}</style><script src=https://npmcdn.com/particlesjs@2.2.2/dist/particles.min.js></script><div class=title>${title}</div><div class=link><a href=https://docs.textile.io target=_blank>Built with Textile</a></div><canvas class=background></canvas><script>window.onload=function(){Particles.init({selector:'.background',color:'#FFB6D4',maxParticles:130,connectParticles:true,responsive:[{breakpoint:768,options:{maxParticles:80}},{breakpoint:375,options:{maxParticles:50}}]});};</script>`
