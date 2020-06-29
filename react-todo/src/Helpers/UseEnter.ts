2014d110ea2e4b58e519d55f84cae4d0fdf90986
Andrew W. Hill <andrew@textile.io>
2020-06-29 12:33:57 -0700

    chore(cleanup): temporarily remove the unmaintained examples (#20)
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- react-todo/src/Helpers/UseEnter.ts
+++
@@ -1,9 +0,0 @@
-import { KeyboardEvent } from "react";
-
-export const onEnterPress = (cb: any) => {
-  return (e: KeyboardEvent) => {
-    if (e.key === "Enter") {
-      cb();
-    }
-  };
-};
--- react-todo/src/Helpers/UseStore.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import { StoreContext } from "./StoreProvider";
-import { TodoList } from "../Stores/TodoList";
-
-export const useStore = (): TodoList => useContext(StoreContext);
