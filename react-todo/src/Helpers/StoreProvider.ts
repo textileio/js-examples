1b9048a8cba3cb70cfa523e6f4fabfa4270a36e4
Andrew Hill <andrew@textile.io>
2020-06-29 12:31:28 -0700

    chore(cleanup): temporarily remove the unmaintained examples
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- react-todo/src/Helpers/StoreProvider.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { TodoList } from "../Stores/TodoList";
-
-export const StoreContext = createContext<TodoList>({} as TodoList);
-export const StoreProvider = StoreContext.Provider;
